b0f398fc386695351aa9ac66c773781a5db28bde
Chibakuz <lllgymslll@gmail.com>
2023-10-06 18:04:24 +0700

    Your commit message here

---
+++ script/state_1.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+#func _process(delta):
+	#$Camera2D.position.x = $Player.position.x
+	#$Camera2D.position.y = $Player.position.y
+
+
+
+func _on_area_2d_area_entered(area):
+	pass # Replace with function body.
