4c5eed076485e68200ede5debc937f214c08a3f4
Jirapong Kongsombutjaroen <nassurmi25@gmail.com>
2023-10-08 14:42:18 +0700

    add bee and goblin but still little unfinished

---
+++ Scene/bee.gd
@@ -0,0 +1,59 @@
+extends CharacterBody2D
+
+var is_moving_left = true
+var gravity = 10
+var speed = 100
+var turn_side
+var health = 100
+@onready var state_machine = $AnimationTree.get("parameters/playback")
+
+func _physics_process(delta):
+	var previous_velocity_x = velocity.x
+
+	if is_on_wall():
+		is_moving_left = !is_moving_left
+		velocity.x = -velocity.x  # Reverse the horizontal velocity
+	# Update velocity based on the current direction
+	velocity.x = -speed if is_moving_left else speed
+	velocity.y += gravity
+
+	move_and_slide()
+	
+func attacked(damage):
+	$TextureProgressBar.visible = true
+	health -= damage
+	$TextureProgressBar.value = health
+	$Timer.wait_time = 1
+	$Timer.start()
+	if health <= 0:
+		state_machine.travel('dead')
+		$TextureProgressBar.visible = false
+		queue_free()
+
+func _on_detech_player_body_entered(body):
+	if body.is_in_group('player'):
+		state_machine.travel("attack")
+		speed = 0
+		
+
+func _on_detech_player_body_exited(body):
+	if body.is_in_group('player'):
+		state_machine.travel("fly")
+		speed = 100
+	
+
+func _on_attack_player_body_entered(body):
+	if body.has_method('attacked'):
+		body.attacked(50)
+	
+	
+func _on_area_2d_area_entered(area):
+	is_moving_left = !is_moving_left
+	scale.x = -scale.x
+
+
+func _on_timer_timeout():
+	$TextureProgressBar.visible = false
+
+
+
